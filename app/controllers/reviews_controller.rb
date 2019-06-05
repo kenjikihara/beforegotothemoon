cdde4316b346e250ad717fa620e468722a4108f3
Kenji Kihara <kenjikihara1220@gmail.com>
2019-06-05 09:53:29 +0900

    update review-page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,9 @@
 class ReviewsController < ApplicationController
   def index
+    @reviews = Review.order(created_at: :desc).page(params[:page]).per(1)
   end
   def show
+    @reviews = Review.order(created_at: :desc).page(params[:page]).per(1)
+    @review = Review.find(params[:id])
   end
 end
